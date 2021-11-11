c28e4a3172c4268dea0a5cfda4de8e7c60cb3d96
Korotin <korotinmain@gmail.com>
2021-11-11 05:45:39 +0200

    Task management api

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,69 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Logger,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateStatusDto } from './dto/update-task-status.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from '../auth/get-user.decorator';
+import { User } from '../auth/user.entity';
+
+@Controller('tasks')
+@UseGuards(AuthGuard())
+export class TasksController {
+  private logger = new Logger('TasksController');
+
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getTasks(
+    @Query() filterDto: GetTasksFilterDto,
+    @GetUser() user: User,
+  ): Promise<Array<Task>> {
+    this.logger.verbose(
+      `User "${user.username}" retrieving all tasks. Filters: ${JSON.stringify(
+        filterDto,
+      )}`,
+    );
+    return this.tasksService.getTasks(filterDto, user);
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string, @GetUser() user: User): Promise<Task> {
+    return this.tasksService.getTaskById(id, user);
+  }
+
+  @Post()
+  createTask(
+    @Body() createTaskDto: CreateTaskDto,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    return this.tasksService.createTask(createTaskDto, user);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string, @GetUser() user: User): Promise<void> {
+    return this.tasksService.deleteTask(id, user);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatues(
+    @Param('id') id: string,
+    @Body() udateStatusDto: UpdateStatusDto,
+    @GetUser() user: User,
+  ): Promise<Task> {
+    const { status } = udateStatusDto;
+    return this.tasksService.updateTaskStatus(id, status, user);
+  }
+}
